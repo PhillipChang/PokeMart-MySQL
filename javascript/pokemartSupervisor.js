04f49faddff27b4cfeebd827f3deb9e2cb0dc15b
Phillip Chang <phillip-chang@live.com>
2019-06-26 23:00:43 -0700

    Establish connection to mysql and load mysql, inquirer and table

---
+++ javascript/pokemartSupervisor.js
@@ -0,0 +1,17 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var Table = require("cli-table2");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"password",
+    database:"pokemartDB"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
