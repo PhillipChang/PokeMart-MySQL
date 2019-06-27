7a5b5ee2e45dcae539c8db3c4f70ac256f375542
Phillip Chang <phillip-chang@live.com>
2019-06-26 22:10:49 -0700

    Create start function and switch statement

--- javascript/pokemartManager.js
+++ javascript/pokemartManager.js
@@ -15,3 +15,37 @@ connection.connect(function(err){
     console.log("connected as id " + connection.threadId + "\n");
     start();
 });
+
+
+function start(){
+    console.log(`     
+     _   _   _   _   _   _   _   _
+    / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\
+   ( P | o | k | e | M | a | r | t )
+    \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/`);
+    inquirer.prompt({
+        type:"rawlist",
+        name:"choice",
+        message: "What would you like to do?",
+        choices:["View Products for Sale","View Low Inventory","Add to Inventory","Add new Product","Exit"]
+    })
+    .then(function(answer){
+        switch(answer.choice){
+            case 'View Products for Sale':
+                viewInv();
+                break;
+            case 'View Low Inventory':
+                viewLowInv();
+                break;
+            case 'Add to Inventory':
+                addInv();
+                break;
+            case 'Add new Product':
+                addNewProd();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    });
+}
