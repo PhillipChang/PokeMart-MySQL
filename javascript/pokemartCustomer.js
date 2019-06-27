46fa3af1f4a247301437ace24062c595b00af034
Phillip Chang <phillip-chang@live.com>
2019-06-26 22:03:31 -0700

    Add validate to buy function and change quantityQuery variable to include product name price and item id

--- javascript/pokemartCustomer.js
+++ javascript/pokemartCustomer.js
@@ -17,6 +17,11 @@ connection.connect(function(err){
 });
 
 function start(){
+//     console.log(`     
+//      _   _   _   _   _   _   _   _
+//     / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\
+//    ( P | o | k | e | M | a | r | t )
+//     \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/`);
     inquirer.prompt({
         type:"rawlist",
         name:"choice",
@@ -42,20 +47,66 @@ function viewInv(){
     var query = "SELECT * FROM products"
     connection.query(query,function(err,res){
         if (err) throw err;
-        var table = new Table({
-            head:["Item ID","Product Name", "Department Name", "Price", "Quantity"]
-            // chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
-            //        , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
-            //        , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
-            //        , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
-          });
+        // console.log(res);
+        var table = new Table({head:["Item ID","Product Name", "Dept Name", "Price", "Quantity"]
+            ,colWidths:[10,20,15,15,10]
+        });
         for (i=0; i<res.length;i++){
             // console.log(res);
             table.push(
                 [res[i].item_id,res[i].product_name,res[i].department_name,res[i].price,res[i].stock_quantity]
             );
         }
-        console.log("\n",table.toString());
+        console.log("\n"+table.toString());
+        start();
     });
-    start();
+}
+
+function buy(){
+    inquirer.prompt([
+        {
+        name:"buyerChoice",
+        type:"input",
+        message:"What item would you like to purchase? (Enter Item ID)",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+    },
+    {
+        name:"quantity",
+        type:"input",
+        message:"How many would you like to purchase?",
+        validate: function(value) {
+            if (isNaN(value) === false) {
+              return true;
+            }
+            return false;
+          }
+    }
+])
+.then(function(answer){
+    var quantityQuery = "SELECT stock_quantity,item_id,price,product_name FROM products WHERE item_id = ?"
+    connection.query(quantityQuery,[answer.buyerChoice],function(error,res){
+        if (error) throw error;
+        var qty = res[0].stock_quantity;
+        var price = res[0].price;
+        var prod = res[0].product_name;
+        if (answer.quantity >= qty){
+            console.log("There is not sufficient inventory to meet your needs.");
+            }
+        else{
+    var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+    qty -= answer.quantity;
+    connection.query(query,[qty,answer.buyerChoice],function(err){
+        if (err) throw err;
+        var total = price * answer.quantity;
+        console.log("You have successfully bought",answer.quantity,prod,"for","$"+total.toFixed(2));
+        });
+        };
+        start();
+    });
+});
 }
--- mysql/pokemart.sql
+++ mysql/pokemart.sql
@@ -5,7 +5,7 @@ CREATE DATABASE pokemartDB;
 USE pokemartDB;
 
 CREATE TABLE products (
-    item_id INT NOT NULL,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50) NULL,
     department_name VARCHAR(50) NULL,
     price DECIMAL(10,2) NULL,
@@ -13,14 +13,14 @@ CREATE TABLE products (
     PRIMARY KEY(item_id)
 );
 
-INSERT INTO products (item_id,product_name,department_name,price,stock_quantity)
-VALUES ("Pokeballs","Equipment",20.00,50),
-("Antidote","Meds",10.00,200),
-("Escape Rope","Equipment",5.50,30),
-("Super Potion","Meds",70.00,25),
-("Rare Candy","Specialty"120.00,5),
-("Ultra Ball","Equipment",120.00,20),
-("Poke Doll","General",10.00,25),
-("Surf Mail","General",10.00,15),
-("Rare Candy Bar","Specialty",30.00, 45);
-("Revive", "Meds",150.00,10);
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("Pokeballs","Equipment",20.99,50),
+("Antidote","Meds",10.25,200),
+("Escape Rope","Equipment",5.35,30),
+("Super Potion","Meds",70.45,25),
+("Rare Candy","Specialty",120.55,5),
+("Ultra Ball","Equipment",120.15,20),
+("Poke Doll","General",9.99,25),
+("Surf Mail","General",9.99,15),
+("Rare Candy Bar","Specialty",25.98, 45),
+("Revive", "Meds",150.29,10);
