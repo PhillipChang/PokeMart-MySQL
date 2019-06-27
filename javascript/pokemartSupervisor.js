f101be240498b3a0bfe8413b46021267cdf9121b
Phillip Chang <phillip-chang@live.com>
2019-06-26 23:02:23 -0700

    Create switch statement and start function

--- javascript/pokemartSupervisor.js
+++ javascript/pokemartSupervisor.js
@@ -15,3 +15,31 @@ connection.connect(function(err){
     console.log("connected as id " + connection.threadId + "\n");
     start();
 });
+
+
+function start(){
+    console.log(`     
+     _   _   _   _   _   _   _   _
+    / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\
+   ( P | o | k | e | M | a | r | t )
+    \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/`);
+    inquirer.prompt({
+        type:"rawlist",
+        name:"choice",
+        message: "How may I help you?",
+        choices:["View Product Sales by Department","Create New Department","Exit"]
+    })
+    .then(function(answer){
+        switch(answer.choice){
+            case 'View Product Sales by Department':
+                viewProdSales();
+                break;
+            case 'Create New Department':
+                newDept();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    });
+}
