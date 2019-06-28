6d60cfd22b7d65df5fafbed445f96295045b5c2d
Phillip Chang <phillip-chang@live.com>
2019-06-27 21:52:19 -0700

    Add default value for product sales

--- mysql/pokemart.sql
+++ mysql/pokemart.sql
@@ -6,18 +6,18 @@ USE pokemartDB;
 
 CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR(50) NULL,
-    department_name VARCHAR(50) NULL,
-    price DECIMAL(10,2) NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL,
-    product_sales DECIMAL(10,2) NULL,
+    product_sales DECIMAL(10,2) DEFAULT 0,
     PRIMARY KEY(item_id)
 );
 
 CREATE TABLE departments (
     department_id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(50) NULL,
-    over_head_costs DECIMAL(10,2) NULL,
+    department_name VARCHAR(50) NOT NULL,
+    over_head_costs DECIMAL(10,2) NOT NULL,
     PRIMARY KEY(department_id)
 );
 
